86d1976d6b0e7ee8adba1f9b327494b7b1bbc8fd
CRITEOIS\s.gallardo <s.gallardo@criteo.com>
2018-03-31 13:26:30 -0400

    Week 9 Assignment - First Commit

---
+++ football_stats.py
@@ -0,0 +1,34 @@
+#!user/bin/env python
+# -*- coding: utf-8 -*-
+""" Week 9 Assignment """
+
+import urllib2
+from bs4 import BeautifulSoup
+
+url = "http://www.cbssports.com/nfl/stats/playersort/nfl/year-2017-season-regular-category-touchdowns"
+page = urllib2.urlopen(url)
+soup = BeautifulSoup(page.read())
+
+trs = soup.find_all('tr')
+top20Counter = 0
+
+for tr in trs:
+    for link in tr.find_all('a'):
+        fulllink = link.get ('href')
+
+
+    tds = tr.find_all("td")
+
+    try:
+        names = str(tds[0].get_text())
+        position = str(tds[1].get_text())
+        team = str(tds[2].get_text())
+        touchdowns = tds[6].get_text()
+
+    except:
+        continue
+    top20Counter += 1
+    if top20Counter > 20:
+        break
+
+    print "Name: {} | Position: {} | Team: {} | Touchdowns {}.".format(names, position, team, touchdowns)
